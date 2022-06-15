dbec786a4a6762709b05d6a72a8861f0a2c57370
alois <ycg02109@naver.com>
2022-06-16 07:48:25 +0900

    FIRST COMMIT

---
+++ src/org/opentutorials/javatutorials/variables/StringDemo.java
@@ -0,0 +1,21 @@
+package org.opentutorials.javatutorials.variables;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		String first; // first라는 String 형식 변수
+		first = "coding";
+		
+		String second;
+		second = "Life";
+		
+		System.out.println(first+ " " + second);
+		
+		String a, b;
+		a = "code";
+		b = "everybody";
+		
+		System.out.println(a+b);
+	}
+
+}
